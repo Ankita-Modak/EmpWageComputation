68527d301a8837a8e6b67a237ed291d1579dcf1e
Ankita-Modak <ankitamodak15@gmail.com>
2023-04-10 20:14:46 +0530

    third commit

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
+            int empHrs = 0;
+            int empWage = 0;
             int isPresent = 1;
+            const int EMP_RATE_PER_HR = 20;
             //welcome message
             Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
             //UC1-EMployeeAttendance
@@ -13,11 +16,16 @@
             if (employeeAttendence == isPresent)
             {
                 Console.WriteLine("employee attendence is present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("employee attendence is absent");
+
             }
+            //UC2
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee wage is:" + empWage);
         }
     }
 }
