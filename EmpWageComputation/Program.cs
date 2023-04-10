b24c4bcd53cacce06cb98de1e77b24ba6551c6e6
Ankita-Modak <ankitamodak15@gmail.com>
2023-04-10 20:07:32 +0530

    second commit

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage program");
+            int isPresent = 1;
+            //welcome message
+            Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
+            //UC1-EMployeeAttendance
+            Random random = new Random();
+            int employeeAttendence = random.Next(0, 2);
+            if (employeeAttendence == isPresent)
+            {
+                Console.WriteLine("employee attendence is present");
+            }
+            else
+            {
+                Console.WriteLine("employee attendence is absent");
+            }
         }
     }
 }
